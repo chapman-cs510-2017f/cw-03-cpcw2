594a5bf6dd86d25f4116d0f1754f10aeb24e36b0
cwatkins1123 <watki115@mail.chapman.edu>
2017-09-13 02:52:50 +0000

    Fibbbbb

--- fib.py
+++ fib.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from sequences import fibonacci
-import sys
+def main(local_argv):
+    print(fibonacci(local_argv[0])[-1])
+if __name__ == "__main__":
+    # This block only executes if the script is run as a standalone
+    # program from the command line. It is not run when imported as
+    # a module.
+    
+    # It is convention to call a single function here if possible
+    # This function should be defined above and house all code to be
+    # executed. Note that sys.argv will contain all commandline options.
+    # The getopt module may also be helpful for more ambitious programs.
+    import sys
+    main(sys.argv[0])
 
-print(fibonacci(sys.argv[0])[-1])
